46aef0974df48d46500bd868e04886854c171478
TazoPsuturi <tazopsuturi33@gmail.com>
2025-03-16 21:56:42 +0400

    added feature file

--- src/test/java/com/epam/training/student_tamaz_psuturi/service/TestDataReader.java
+++ src/test/java/com/epam/training/student_tamaz_psuturi/service/TestDataReader.java
@@ -3,7 +3,7 @@ package com.epam.training.student_tamaz_psuturi.service;
 import java.util.ResourceBundle;
 
 public class TestDataReader {
-	private static ResourceBundle resourceBundle = ResourceBundle.getBundle(System.getProperty("environment"));
+	private static final ResourceBundle resourceBundle = ResourceBundle.getBundle(System.getProperty("environment"));
 	
 	public static String getTestData(String key) {
 		return resourceBundle.getString(key);
