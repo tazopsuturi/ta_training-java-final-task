4d872a85ca42365dab287f88baa1a089b86c31f8
TazoPsuturi <tazopsuturi33@gmail.com>
2025-03-20 19:24:32 +0400

    Changed to hamcrest.assert

--- src/test/java/com/epam/training/student_tamaz_psuturi/test/AccessWithoutPasswordTest.java
+++ src/test/java/com/epam/training/student_tamaz_psuturi/test/AccessWithoutPasswordTest.java
@@ -3,8 +3,10 @@ package com.epam.training.student_tamaz_psuturi.test;
 import com.epam.training.student_tamaz_psuturi.model.User;
 import com.epam.training.student_tamaz_psuturi.page.LoginPage;
 import com.epam.training.student_tamaz_psuturi.service.UserCreator;
-import org.junit.Assert;
 import org.junit.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.containsString;
 
 public class AccessWithoutPasswordTest extends CommonConditions {
 	@Test
@@ -18,6 +20,6 @@ public class AccessWithoutPasswordTest extends CommonConditions {
 				.clickLoginButton()
 				.errorMessageString();
 		
-		Assert.assertTrue(passwordError.contains("Password is required"));
+		assertThat(passwordError, containsString("Password is required"));
 	}
 }
--- src/test/java/com/epam/training/student_tamaz_psuturi/test/UserAccessTest.java
+++ src/test/java/com/epam/training/student_tamaz_psuturi/test/UserAccessTest.java
@@ -5,7 +5,8 @@ import com.epam.training.student_tamaz_psuturi.page.LoginPage;
 import com.epam.training.student_tamaz_psuturi.service.UserCreator;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.junit.Assert;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.containsString;
 import org.junit.Test;
 
 public class UserAccessTest extends CommonConditions {
@@ -22,7 +23,7 @@ public class UserAccessTest extends CommonConditions {
 				.getPageTitle();
 		
 		logger.info("Login attempted. Checking assertions.");
-		Assert.assertEquals(loggedInUserName, "Swag Labs");
+		assertThat(loggedInUserName, containsString("Swag Labs"));
 		
 		logger.info("Test passed: User successfully logged in.");
 	}
